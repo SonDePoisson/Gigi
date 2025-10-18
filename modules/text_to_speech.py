1f7b2846c7612c8a5adbd316a46dad0aa46f679d
SonDePoisson <clement.poissoncornu@gmail.com>
2025-10-19 00:17:27 +0200

    Change TTS for gTTS

--- modules/text_to_speech.py
+++ modules/text_to_speech.py
@@ -1,24 +1,39 @@
-from config import ENV_PATH
-from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-from elevenlabs.play import play
-import os
+# from config import ENV_PATH
+# from dotenv import load_dotenv
+# from elevenlabs.client import ElevenLabs
+# from elevenlabs.play import play
+# import os
 
 
-def init_tts() -> ElevenLabs:
-    load_dotenv(ENV_PATH)
-    elevenlabs = ElevenLabs(
-        api_key=os.getenv("ELEVENLABS_API_KEY"),
-    )
-    return elevenlabs
+# def init_tts() -> ElevenLabs:
+#     load_dotenv(ENV_PATH)
+#     elevenlabs = ElevenLabs(
+#         api_key=os.getenv("ELEVENLABS_API_KEY"),
+#     )
+#     return elevenlabs
 
 
-def speak(text: str, elevenlabs: ElevenLabs, voice: str, model: str):
-    audio = elevenlabs.text_to_speech.convert(
-        text=text,
-        voice_id=voice,
-        model_id=model,
-        output_format="mp3_44100_128",
-    )
+# def speak(text: str, elevenlabs: ElevenLabs, voice: str, model: str):
+#     audio = elevenlabs.text_to_speech.convert(
+#         text=text,
+#         voice_id=voice,
+#         model_id=model,
+#         output_format="mp3_44100_128",
+#     )
 
-    play(audio)
+#     play(audio)
+
+from gtts import gTTS
+import tempfile
+import playsound
+
+
+def speak(text: str, lang: str = "fr"):
+    """Convertit le texte en audio et le joue en français par défaut."""
+    tts = gTTS(text=text, lang=lang)
+
+    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as f:
+        tts.save(f.name)
+        path = f.name
+
+    playsound.playsound(path, block=True)
