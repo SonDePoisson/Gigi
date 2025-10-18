ee19a65c3080200e3943026bb36f03677660c841
SonDePoisson <clement.poissoncornu@gmail.com>
2025-10-19 00:47:06 +0200

    Add choice between ElevenLabs / gTTS

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from modules.speech_to_text import transcribe, init_asr
-from modules.text_to_speech import speak
+from modules.text_to_speech import speak, init_tts
 from modules.brain import generate_response
 from config import BAD_PATTERNS, SAMPLE_RATE, FLASH_2_5_MODEL, VIKING_ID
 
@@ -11,7 +11,7 @@ def main():
     recognizer = sr.Recognizer()
     mic = sr.Microphone(sample_rate=SAMPLE_RATE)
     asr_pipeline = init_asr()
-    # elevenlab_pipe = init_tts()
+    elevenlab_pipe = init_tts()  # Add 'Eleven' to use ElevenLabs
 
     print("Listening... (press Ctrl+C to stop)")
 
@@ -33,8 +33,8 @@ def main():
                     answer = generate_response(text)
                     print(f"AI: {answer}")
                     print("[Speaking]")
-                    # speak(answer, elevenlab_pipe, VIKING_ID, FLASH_2_5_MODEL)
-                    speak(answer)
+                    speak(answer, elevenlab_pipe, VIKING_ID, FLASH_2_5_MODEL)
+                    # speak(answer)
 
             except KeyboardInterrupt:
                 print("\nListening stopped.")
--- modules/text_to_speech.py
+++ modules/text_to_speech.py
@@ -1,39 +1,42 @@
-# from config import ENV_PATH
-# from dotenv import load_dotenv
-# from elevenlabs.client import ElevenLabs
-# from elevenlabs.play import play
-# import os
-
-
-# def init_tts() -> ElevenLabs:
-#     load_dotenv(ENV_PATH)
-#     elevenlabs = ElevenLabs(
-#         api_key=os.getenv("ELEVENLABS_API_KEY"),
-#     )
-#     return elevenlabs
-
-
-# def speak(text: str, elevenlabs: ElevenLabs, voice: str, model: str):
-#     audio = elevenlabs.text_to_speech.convert(
-#         text=text,
-#         voice_id=voice,
-#         model_id=model,
-#         output_format="mp3_44100_128",
-#     )
-
-#     play(audio)
-
+from config import ENV_PATH
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.play import play
+import os
 from gtts import gTTS
 import tempfile
 import playsound
 
 
-def speak(text: str, lang: str = "fr"):
-    """Convertit le texte en audio et le joue en français par défaut."""
-    tts = gTTS(text=text, lang=lang)
+def init_tts(TTS: str = None) -> ElevenLabs | None:
+    if TTS == "Eleven":
+        load_dotenv(ENV_PATH)
+        elevenlabs = ElevenLabs(
+            api_key=os.getenv("ELEVENLABS_API_KEY"),
+        )
+        print("[TTS] : ElevenLabs")
+    else:
+        elevenlabs = None
+        print("[TTS] : gTTS")
+    return elevenlabs
 
-    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as f:
-        tts.save(f.name)
-        path = f.name
 
-    playsound.playsound(path, block=True)
+def speak(
+    text: str, elevenlabs: ElevenLabs | None, voice: str = None, model: str = None
+):
+    if elevenlabs is None:
+        tts = gTTS(text=text, lang="fr")
+
+        with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as f:
+            tts.save(f.name)
+            path = f.name
+
+        playsound.playsound(path, block=True)
+    else:
+        audio = elevenlabs.text_to_speech.convert(
+            text=text,
+            voice_id=voice,
+            model_id=model,
+            output_format="mp3_44100_128",
+        )
+        play(audio)
